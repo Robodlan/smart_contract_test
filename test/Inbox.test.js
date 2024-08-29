7032fe42b0a75bb58529478e87dd064d275a973e
Robodlan <Robodlan@users.noreply.github.com>
2024-08-29 14:15:52 -0400

    typo in methods

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,21 +2,19 @@ const ganache = require("ganache");
 const { Web3 } = require("web3");
 const assert = require("assert");
 const { interface, bytecode } = require("../compile");
-// const ganache = require("ganache");
-// const { Web3 } = require("web3");
-// const assert = require("assert");
+
 const web3 = new Web3(ganache.provider());
 
 let accounts;
 let inbox;
-const initial_string = "Hi there";
+
 beforeEach(async () => {
   // Get a list of accounts
   accounts = await web3.eth.getAccounts();
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: [initial_string],
+      arguments: ["Hi there"],
     })
     .send({ from: accounts[0], gas: "1000000" });
 });
@@ -26,8 +24,8 @@ describe("Inbox", () => {
     assert.ok(inbox.options.address);
   });
   it("has a default message", async () => {
-    const message = await inbox.method.message().call();
-    assert.equal(message, initial_string);
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there");
   });
 });
 
